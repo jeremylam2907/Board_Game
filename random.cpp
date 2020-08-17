5c9f22baadae2c55a1731c51f92d3f4cb7787c04
jeremylam2907 <59300941+jeremylam2907@users.noreply.github.com>
2020-08-16 20:50:49 -0500

    Update random.cpp

--- random.cpp
+++ random.cpp
@@ -6,7 +6,6 @@
 
 /*TODO:
 Add in constraints of game board
-
 Add in constraints of blocking
 Add in non-step over opponent function
 Repeated entry bug
@@ -17,10 +16,9 @@ using namespace std;
 class point{
 public:
     char d;
-    int x;
-    int y;
     int steps = 20;
     int change;
+    string emblem;
 };
 
 //The game table
@@ -54,44 +52,56 @@ void block(){
 }
 
 //Player1's turn
-int gridchange1(char Direction1, int StepsLeft){
+int gridchange(char Direction, int StepsLeft, string piece){
     
-    //Player1 inheritance
-    point player1;
-
-    //Declare position of player 1
-    player1.y;
-    player1.x;
+    int x;
+    int y;
     
     //Find where Player1 is on the grid
     for(int i = 0; i < table.size(); ++i){
         for(int z = 0; z < table[i].size(); ++z){
-            if(table[i][z] == " x"){
-                player1.y = i;
-                player1.x = z;
+            if(table[i][z] == piece){
+                y = i;
+                x = z;
             }
         }
     }
     
     //Replacement after moving forward
-    if(player1.y == 1 && player1.x==3)
-        table[player1.y][player1.x] = "__"; //Temporary to be removed later on
+    if(y == 1 && x==3)
+        table[y][x] = "__"; //Temporary to be removed later on
     else
-        table[player1.y][player1.x] = "[]";
+        table[y][x] = "[]";
 
     //Change direction
-    switch(Direction1){
+    switch(Direction){
         case 'w':
-            table[player1.y - 1][player1.x] = " x";
+            if(table[y - 1][x] == "B "){
+                table[y][x] = piece;
+                break;
+            }
+            table[y - 1][x] = piece;
             break;
         case 's':
-            table[player1.y + 1][player1.x] = " x";
+            if(table[y + 1][x] == "B "){
+                table[y][x] = piece;
+                break;
+            }
+            table[y + 1][x] = piece;
             break;
         case 'a':
-            table[player1.y][player1.x - 1] = " x";
+            if(table[y][x-1] == "B "){
+                table[y][x] = piece;
+                break;
+            }
+            table[y][x - 1] = piece;
             break;
         case 'd':
-            table[player1.y][player1.x + 1] = " x";
+            if(table[y][x+1] == "B "){
+                table[y][x] = piece;
+                break;
+            }
+            table[y][x + 1] = piece;
             break;
     }
 
@@ -100,69 +110,19 @@ int gridchange1(char Direction1, int StepsLeft){
     //Print the updated grid
     grid();
 
-    return(player1.x == 3 && player1.y == 1)? 1:0;
+    return (y == 1 && x==3)? 1:0;
 }
 
-//Player2's turn
-int gridchange2(char Direction2, int StepsLeft){
-    
-    //Player2 inheritance
-    point player2;
 
-    //Declare position of player 2
-    player2.y;
-    player2.x;
-
-    //Find where Player2 is on the grid
-    for(int i = 0; i < table.size(); ++i){
-        for(int z = 0; z < table[i].size(); ++z){
-            if(table[i][z] == "o "){
-                player2.y = i;
-                player2.x = z;
-            }
-        }
-    }
-    
-    //Replacement after moving forward
-    if(player2.y == 1 && player2.x==3)
-        table[player2.y][player2.x] = "__"; //Temporary to be removed later on
-    else
-        table[player2.y][player2.x] = "[]";
-
-    //Change position based on input
-    switch(Direction2){
-        case 'w':
-            table[player2.y - 1][player2.x] = "o ";
-            break;
-        case 's':
-            table[player2.y + 1][player2.x] = "o ";
-            break;
-        case 'a':
-            table[player2.y][player2.x - 1] = "o ";
-            break;
-        case 'd':
-            table[player2.y][player2.x + 1] = "o ";
-            break;
-    }
-
-    cout << endl;
-
-    //Print out the updated grid
-    grid();
-
-    return(player2.x == 3 && player2.y == 1)? 1:0;
-}
 
 int main(){
     //Player1 declaration
     point player1;
-    player1.d;
-    player1.steps;
+    player1.emblem = " x";
 
     //Player2 declaration
     point player2;
-    player2.d;
-    player2.steps;
+    player2.emblem = "o ";
     
     //Call for grid display
     grid();
@@ -171,7 +131,7 @@ int main(){
     while(player1.steps > 0|| player2.steps > 0){
         
         //Player 1's turn
-        cout<< endl << "Do you want to block[enter 1] or move [enter 2]?" << endl;
+        cout<< endl << "Player1, do you want to block[enter 1] or move [enter 2]?" << endl;
         cin >> player1.change;
         if(player1.change == 1){
             block();
@@ -179,11 +139,11 @@ int main(){
         else if(player1.change == 2){
             cin >> player1.d;
             cout << endl;
-            gridchange1(player1.d,player1.steps);
+            gridchange(player1.d,player1.steps,player1.emblem);
         }
         
         //Player 2's turn
-        cout<< endl << "Do you want to block[enter 1] or move [enter 2]?" << endl;
+        cout<< endl << "Player2, do you want to block[enter 1] or move [enter 2]?" << endl;
         cin >> player2.change;
         if(player2.change == 1){
             block();
@@ -191,7 +151,7 @@ int main(){
         else if(player2.change == 2){
             cin >> player2.d;
             cout << endl;
-            gridchange1(player2.d,player2.steps);
+            gridchange(player2.d,player2.steps,player2.emblem);
         }
     }
 }
